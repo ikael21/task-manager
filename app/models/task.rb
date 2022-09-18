575f8420fc22d8d568834bd01a9864cbf8aa2bd5
ikael <ikael.fess@gmail.com>
2022-09-18 19:37:33 +0300

    code refactoring

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,8 +4,6 @@ class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :author, presence: true
-  validates :description, length: { maximum: 500 }
+  validates_presence_of :name, :author, :description
+  validates_length_of :description, maximum: 500
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_secure_password
-
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
@@ -10,4 +8,6 @@ class User < ApplicationRecord
   validates_length_of :first_name, :last_name, minimum: 2
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates_uniqueness_of :email
+
+  has_secure_password
 end
