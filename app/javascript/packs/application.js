bda2bd2b0ae624d4d2ca9bb93ad5d99609261de5
ikael <ikael.fess@gmail.com>
2022-11-06 15:06:44 +0300

    production setup number 3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "styles/application"
+import "stylesheets/application"
 import "material-design-lite/material.js";
 
 Rails.start()
